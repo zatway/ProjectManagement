375fb8df92fed3f1b8f024e24818a52136288e36
zatway <radmirar00@mail.ru>
2025-12-04 20:39:32 +0500

    added auto generate migrations

--- Api/Program.cs
+++ Api/Program.cs
@@ -191,6 +191,12 @@ builder.Services.AddSwaggerGen(c =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ProjectManagementDbContext>();
+    db.Database.Migrate();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
